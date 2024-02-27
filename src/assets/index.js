7a3f79351df20a4e3020075ba2c7b3547b3ea309
LavanyaGokhale <92771328+LavanyaG23@users.noreply.github.com>
2024-02-27 18:08:26 +0530

    added icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -5,11 +5,17 @@ import logoImage from './logoimage.png';
 import image1 from './image1.png';
 import image2 from './image2.png';
 import image3 from './image3.png';
+import CallImage from './CallImage.png';
+import FacebookImage from './FacebookImage.png';
+import globeImage from './globeImage.png';
 
 // Export the image paths
 export {
     logoImage,
     image1,
     image2,
-    image3
+    image3,
+    CallImage,
+    FacebookImage,
+    globeImage
 };
